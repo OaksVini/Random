c2d5bf90c791f5805f4a124117f1532c55ff9543
OaksVini <44732605+OaksVini@users.noreply.github.com>
2018-11-06 14:15:55 -0200

    Update Explicacao.cs

--- Explicacao.cs
+++ Explicacao.cs
@@ -4,20 +4,16 @@
  
 
 // 1# Dentro da sua classe principal você instancia o random.
-
 Random (Nome qualquer) = new Random();
 
 
 // 2# Em seguida você cria um array, sendo 'n' o tamanho do array.
-
 int[] (Nome do array) = new Int[n];
 
 
 // 3# Depois você chama o next dentro do random, para criar os numeros aleatorios.
 // Sendo 'min' o menor numero e 'max' o maior numero.
-
 random.Next(min, max);
-
 // OBS: Para salver esses numeros gerados dentro de uma variavel e manda-los para
 // Dentro do array, continue lendo.
 
@@ -25,7 +21,6 @@ random.Next(min, max);
 // 4# Agora você cria um 'for' para salvar os numeros aleatorios dentro do vetor.
 // Explicando o for: Cria int i, define i = 0, enquanto i for menor que o tamanho do vetor.
 // Faça i++ que seria i + i.
-
 for (int i = 0; i < (tamanho do vetor); i++)
 {
   
@@ -36,7 +31,6 @@ for (int i = 0; i < (tamanho do vetor); i++)
 // Primeiro você define uma variavel pros numeros aleatorios criados.
 // Em seguida chama o vetor e no lugar do indice coloca 'i'
 // Depois você define o valor da posição desse vetor, igual ao numero aleatorio.
-
 int (nome qualquer pros numeros aleatorios) = random.Next(min, max);
 (nome do vetor)[i] = (nome da variavel criada acima);
 
@@ -48,7 +42,6 @@ int (nome qualquer pros numeros aleatorios) = random.Next(min, max);
 
 
 // EXEMPLO
-
 
 public void RandomNumeros()
 {
